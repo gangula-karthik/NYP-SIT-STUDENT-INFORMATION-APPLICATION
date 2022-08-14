8bcd75e10686082d3264a3c3dba330274e8cdc03
gangula-karthik <gkarthik923@gmail.com>
2022-08-14 10:39:27 +0800

    VERSION 2

--- assignment2.py
+++ assignment2.py
@@ -7,6 +7,7 @@
 # When function finishes executing then the program comes back to this menu
 
 
+
 from student_functions import *
 
 
@@ -41,7 +42,7 @@ while True:
     elif choice == 2: 
         print("== Update student ==")
         admin_No_Updater = input("Please enter the admission number of the student you would like to update: ")
-        if adminExists(admin_No_Updater) == True:
+        if isAdminExists(admin_No_Updater) == True:
             print(f"Admission number: {admin_No_Updater}")
             print(f"Student name: {studentDatabase[admin_No_Updater][0]}")
             print(f"Module name: {studentDatabase[admin_No_Updater][1]}")
@@ -53,21 +54,24 @@ Enter 2 to Update module name
 Enter 3 to Update score
 Enter 0 to return to main menu 
             """)
-            print(updateStudent(admin_No_Updater))
+            updater = int(input("What would you like to update? "))
+            print(updateStudent(admin_No_Updater, updater))
         else:
-            print(adminExists(admin_No_Updater))
+            print(isAdminExists(admin_No_Updater))
 
 
     elif choice == 3: 
         print("== Remove student ==")
         admin_no_remover = input("Please enter the admission number you would like to remove: ")
-        if adminExists(admin_no_remover) == True:
+        if isAdminExists(admin_no_remover) == True:
             print(f"Admission number: {admin_no_remover}")
             print(f"Student name: {studentDatabase[admin_no_remover][0]}")
             print(f"Module name: {studentDatabase[admin_no_remover][1]}")
             print(f"score: {studentDatabase[admin_no_remover][2]}")
 
-        print(removeStudent(admin_no_remover))
+        remove_confirmation = input("Is this the student you want to remove? (Yes/No): ")
+
+        print(removeStudent(admin_no_remover, remove_confirmation))
 
 
     elif choice == 4:
@@ -84,10 +88,39 @@ Enter 0 to return to main menu
         Enter 0 to return to main menu
         """)
         searchChoice = int(input("Your choice: "))
-        searchStudent(searchChoice)
+
+
+        if searchChoice == 1: 
+            admissionNumber = input("Please enter the admission number you wish to search by: ")
+            print(searchData(searchChoice, admissionNumber))
+        elif searchChoice == 2: 
+            moduleName = input("Please enter the module name you wish to search by: ")
+            print(searchData(searchChoice, moduleName))
+        elif searchChoice == 3: 
+            min_score = int(input("Minimum score: "))
+            max_score = int(input("Maximum score: "))
+            scoreLst = [min_score, max_score]
+            print(searchData(searchChoice, scoreLst))
+        elif searchChoice == 0: 
+            print("")
+
+        
+        # if searchChoice == 1: 
+        #     admin_no_searcher = input("Please enter the admission number you wish to search by: ")
+        #     if adminExists(admin_no_searcher) == True: 
+        #         print(f"Admission Number: {admin_no_searcher}")
+        #         print(f"Student Name: {studentDatabase[admin_no_searcher][0]}")
+        #         print(f"Module Name: {studentDatabase[admin_no_searcher][1]}")
+        #         print(f"score: {studentDatabase[admin_no_searcher][2]}")
+        #     elif adminExists(admin_no_searcher) != True: 
+        #         print("No student with this Admission number was found")
+
+                
+        # elif searchChoice == 2: 
+        #     module_name_searcher = input("Please enter the module name you wish to search by: ")
+        #     searchModule(module_name_searcher)
 
 
     elif choice == -1: 
         break
-
 
--- student_functions.py
+++ student_functions.py
@@ -7,21 +7,19 @@ def addStudent(admissionNumber, studentName, moduleName, score):
     studentDatabase[admissionNumber] = [studentName, moduleName, score]
     return "Student added!"
 
-
-
-def adminExists(admin_no):
+def isAdminExists(admin_no):
     if admin_no in studentDatabase:
         return True
     elif admin_no not in studentDatabase:
         return "No such student found."
 
+def isStudenDBEmpty():
+    if len(studentDatabase) != 0:
+        return True
 
-def updateStudent(admin_No_Updater):
+def updateStudent(admin_No_Updater, updater):
     global studentDatabase
-    if len(studentDatabase) == 0:
-        return "There is currently no student information in the system"
-    else:
-        updater = int(input("What would you like to update? "))
+    if isStudenDBEmpty():
         if updater == 1: 
             new_name_student = input("Enter the new student name: ")
             studentDatabase[admin_No_Updater][0] = new_name_student
@@ -38,13 +36,12 @@ def updateStudent(admin_No_Updater):
             return ""
 
 
-def removeStudent(admin_no_remover):
+def removeStudent(admin_no_remover, remove_student):
     global studentDatabase
-    remove_confirmation = input("Is this the student you want to remove? (Yes/No): ")
-    if remove_confirmation.lower() == "yes":
+    if remove_student.lower() == "yes":
         del studentDatabase[admin_no_remover]
         return "student information removed!"
-    elif remove_confirmation.lower() == "no":
+    elif remove_student.lower() == "no":
         return "student information was not removed!"
 
 
@@ -60,60 +57,72 @@ Module name: {students[i][1]}
 Student name: {students[i][2]}
 """)
 
+def searchData(id, value):
+    searchresults = {}
+    if id == 1:
+        searchresults = (f"""
+Admission number: {value}
+Student name: {studentDatabase[value][0]}
+Module name: {studentDatabase[value][1]}
+Student name: {studentDatabase[value][2]}
+""")
+        return searchresults
+    elif id == 2: 
+        searchresults = {i: studentDatabase[i] for i in studentDatabase if studentDatabase[i][1] == value}
+        return searchresults
 
-def searchStudent(searchChoice):
-    global studentDatabase
-    if searchChoice == 1:
-        admin_no_searcher = input("Please enter the admission number you wish to search by: ")
-        def searchAdmin(admin_no):
-            if admin_no in studentDatabase: 
-                print(f"Admission Number: {admin_no}")
-                print(f"Student Name: {studentDatabase[admin_no][0]}")
-                print(f"Module Name: {studentDatabase[admin_no][1]}")
-                print(f"score: {studentDatabase[admin_no][2]}")
-            elif admin_no not in studentDatabase: 
-                print("No student with this Admission number was found")
+    elif id == 3: 
+        searchresults = {i: studentDatabase[i] for i in studentDatabase if value[0] <= studentDatabase[i][2] <= value[1]}
+        return searchresults
 
-        searchAdmin(admin_no_searcher)
-    
-    elif searchChoice == 2: 
-        module_searcher = input("Please enter the module name you wish to search by: ")
-        def searchModule(module):
-            module_lst = [i for i in studentDatabase if studentDatabase[i][1] == module]
-            if len(module_lst) != 0:
-                for i in studentDatabase:
-                    print()
-                    if studentDatabase[i][1] == module:
-                        print(f"Admission number: {i}")
-                        print(f"Student name: {studentDatabase[i][0]}")
-                        print(f"Module name: {studentDatabase[i][1]}")
-                        print(f"score: {studentDatabase[i][2]}")
-            else:
-                print("No student with this module name was found")
-            print()
-        searchModule(module_searcher)
-
-
-    elif searchChoice == 3: 
-        print("Please enter the range of score you wish to search by: ")
-        min_score = int(input("Minimum score: "))
-        max_score = int(input("Maximum score: "))
-
-        def searchScore(min_score, max_score):
-            score_lst = [i for i in studentDatabase if min_score <= studentDatabase[i][2] <= max_score]
-            if len(score_lst) != 0:
-                for i in studentDatabase:
-                    print()
-                    if min_score <= studentDatabase[i][2] <= max_score:
-                        print(f"Admission number: {i}")
-                        print(f"Student name: {studentDatabase[i][0]}")
-                        print(f"Module name: {studentDatabase[i][1]}")
-                        print(f"score: {studentDatabase[i][2]}")
-            else:
-                print("No student with this score was found")
-            print()
-
-        searchScore(min_score, max_score)
-
-    elif searchChoice == 0:
+    elif id == 0:
         return None
+
+    else: 
+        return "Invalid input"
+
+
+
+
+
+
+# module_searcher = input("Please enter the module name you wish to search by: ")
+# def searchModule(module):
+#     module_lst = [i for i in studentDatabase if studentDatabase[i][1] == module]
+#     if len(module_lst) != 0:
+#         for i in studentDatabase:
+#             print()
+#             if studentDatabase[i][1] == module:
+#                 print(f"Admission number: {i}")
+#                 print(f"Student name: {studentDatabase[i][0]}")
+#                 print(f"Module name: {studentDatabase[i][1]}")
+#                 print(f"score: {studentDatabase[i][2]}")
+#     else:
+#         print("No student with this module name was found")
+#     print()
+# searchModule(module_searcher)
+
+
+    # elif searchChoice == 3: 
+    #     print("Please enter the range of score you wish to search by: ")
+    #     min_score = int(input("Minimum score: "))
+    #     max_score = int(input("Maximum score: "))
+
+    #     def searchScore(min_score, max_score):
+    #         score_lst = [i for i in studentDatabase if min_score <= studentDatabase[i][2] <= max_score]
+    #         if len(score_lst) != 0:
+    #             for i in studentDatabase:
+    #                 print()
+    #                 if min_score <= studentDatabase[i][2] <= max_score:
+    #                     print(f"Admission number: {i}")
+    #                     print(f"Student name: {studentDatabase[i][0]}")
+    #                     print(f"Module name: {studentDatabase[i][1]}")
+    #                     print(f"score: {studentDatabase[i][2]}")
+    #         else:
+    #             print("No student with this score was found")
+    #         print()
+
+    #     searchScore(min_score, max_score)
+
+    # elif searchChoice == 0:
+    #     return None
